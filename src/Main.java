6886d3b36382e2ecdc32da9357ad4dd4bc26ab35
varvaragr <grechishnikova.varvara@gmail.com>
2024-06-04 08:12:04 +0300

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+
 
     }
 }
